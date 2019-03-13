1fc8e3bd79dab3bff512c502f47023a8aa921aa9
KeniSP <36774433+KeniSP@users.noreply.github.com>
2019-03-12 21:57:36 -0700

    Update salary_to_hourly.py

--- salary_to_hourly.py
+++ salary_to_hourly.py
@@ -1,52 +1,25 @@
 #!/usr/local/bin/python3
 #Calculating your yearly pay into bi weekky and hourly rates
 
-import sys
-import os
-
-#restarts script on error instead of breaking
-def letter_error():
-    print("numbers only, no commas or letters, restarting")
-    os.execv(__file__, sys.argv)
-
-
-yearly_salary = float(input("Enter Yearly Salary: "))
-try:
-    yearly_salary_ival = int(yearly_salary)
-except:
-    yearly_salary_ival = -1
-    if yearly_salary_ival > 0:
-        print("Thanks, moving on")
+while True:
+    print()
+    yearly_salary = float(input("Enter Yearly Salary: "))
+    extra_income_question = input("Do you want to include a extra income?: ")
+    if extra_income_question in ("y", "yes", "Yes", "Y"):
+        extra_income = input("Enter Total Extra Income: ")
+        try:
+            extra_income = float(extra_income)
+        except ValueError:
+            print("Extra Income was an invalid number")
+            extra_income = 0.0
     else:
-        letter_error()
+        extra_income = 0.0
 
-# try for int that way i confirm value is a number, if not restart script
+    #math below is based on 26 paychecks a year and a 40 hour work week. Aka 80 hours in 2 weeks
 
-extra_income_question = input("Do you want to include a extra income?: ")
-if extra_income_question in ("Yes", "yes", "y", "Y"):
-    extra_income = input("Enter Bonus: ")
-    try:
-        extra_income_ival = int(float(extra_income))
-    except:
-        extra_income_ival = -1
-        if extra_income_ival > 0:
-            print(" ")
-        else:
-            letter_error()
-elif extra_income_question in ("No", "no", "n", "N"):
-    extra_income = 0.0
-else:
-    print("Expecting 'Yes' or 'No', restarting")
-    os.execv(__file__, sys.argv)
-
-
-#math below is based on 26 paychecks a year and a 40 hour work week. Aka 80 hours in 2 weeks
-
-total_pay = float(yearly_salary) + float(extra_income)
-print("Total Yearly Pay:" , total_pay)
-biweekly_salary = float(yearly_salary) / float(26)
-hourly_salary = float(biweekly_salary) / float(80)
-print("You make:" , round(biweekly_salary) , "dollars Bi Weekly before tax")
-print("You make:" , round(hourly_salary) , "dollars an hour before tax")
-print("Restarting for next use")
-os.execv(__file__, sys.argv)
+    total_pay = yearly_salary + extra_income
+    print("Total Yearly Pay:" , total_pay)
+    biweekly_salary = yearly_salary / 26
+    hourly_salary = biweekly_salary / 80
+    print("You make:" , round(biweekly_salary) , "dollars Bi Weekly before tax")
+    print("You make:" , round(hourly_salary) , "dollars an hour before tax")
